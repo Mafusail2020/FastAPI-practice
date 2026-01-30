475f2e8e07546e251fb27a13e394cfc294e218fe
Mafusail2020 <krutkev00@gmail.com>
2026-01-30 19:07:15 +0200

    Add basic router example

---
+++ basic_ex.py
@@ -0,0 +1,20 @@
+from typing import Union, Any
+
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: Any, q: Union[str, None] = None, k: Union[str, None] = None):
+    return {"item_id": item_id, "q": q, "k": k}
+
+
+if __name__ == "__main__":
+    uvicorn.run("basic_ex:app", host="0.0.0.0", port=8000, reload=True)
