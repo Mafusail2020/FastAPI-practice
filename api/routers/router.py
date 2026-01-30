f97c4a21c03638a23e7bb0eaea2ce9e251461978
Mafusail2020 <krutkev00@gmail.com>
2026-01-30 22:11:02 +0200

    Add methods to router

--- api/routers/router.py
+++ api/routers/router.py
@@ -1,4 +1,10 @@
 from fastapi import APIRouter
+from routers.schema import (
+    Item, 
+    ListItems,
+    ItemCreateRequest,
+    ItemCreateResponce
+)
 
 
 router = APIRouter()
@@ -8,14 +14,38 @@ async def root():
     print("Got a root router request!")
     return {"message": "Hello World"}
 
+# GET ITEMS
+@router.get("/list", response_model=ListItems)
+async def get_items_list():
+    or_like_this = Item(id=2)
 
-@router.post("/")
-async def root(data: str):
-    print(f"Got POST on router root!\nData sent: {data}")
-    return {"responce": "Ok!", "data_responce": data}
+    return {
+        "field": [
+        {"id": 1}, 
+        or_like_this
+        ], # both are passing, because both meet Item schema List[Item]
+        "is_free": True
+    } 
 
 
 @router.get("/{some_id}")
-async def get_some_id(some_id: int):
+async def get_some_id(some_id: int)->Item:
     print("Get_id")
+    return {"id": some_id}
+
+
+# SET ITEMS
+@router.post("/create_id", response_model=ItemCreateResponce)
+async def set_some_id(payload: ItemCreateRequest):
+    print(payload)
+
+    data = payload.model_dump() # payload -> dict
+    print(data)
+    return {"id": 123, **data}
+
+
+# PUT ITEMS
+@router.put("/{some_id}", response_model=Item)
+async def update_id(some_id: int, data: dict):
+    print(data)
     return {"id": some_id}
